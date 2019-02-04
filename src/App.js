654b8aa7252be14a5a885dce7e2f6985f6ab568b
Kevin Zhang <kzhang@federatedwireless.com>
2019-02-04 12:50:09 -0500

    help

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+	<>
+		<div
+			className="App"
+		>
+			asdf
+		</div>
+		<div
+			className="App"
+		>
+			<>asdf</>
+		</div>
+	</>
+);
+
+export default App;
